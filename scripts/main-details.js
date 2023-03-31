04ae9c3876deb8eb713ebd10204a9516fc637818
Ezequiel <73133884+ezecastrog@users.noreply.github.com>
2023-03-31 15:28:56 -0300

    :sparkles: feature, index, pastEvents and upcomingEvents are now working with Vue

--- scripts/main-details.js
+++ scripts/main-details.js
@@ -1,43 +1,79 @@
-let urlAPI = 'https://mindhub-xj03.onrender.com/api/amazing';
-let events = [];
-let currentDate = '';
-const contenedorTarjeta = document.getElementById('contenedorTarjeta') 
-let query = location.search
-let params = new URLSearchParams(query)
-let idParams = params.get("id")
+// let urlAPI = 'https://mindhub-xj03.onrender.com/api/amazing';
+// let events = [];
+// let currentDate = '';
+// const contenedorTarjeta = document.getElementById('contenedorTarjeta') 
+// let query = location.search
+// let params = new URLSearchParams(query)
+// let idParams = params.get("id")
 
-//Llamada de funciones
-getEvents();
+// //Llamada de funciones
+// getEvents();
 
-// Funciones
-function tarjetaSeleccionada(){
-  let tarjetaElegida = events.find(info => info._id == idParams);
-  let tarjeta = "";
-  tarjeta += `<img src="${tarjetaElegida.image}" class="card-img-top d-flex" alt="..." height="280" >
-  </div>
-  <div class="p-3"></div>
-  <div class="card justify-content-center border-dark p-2  d-flex">
-      <h2 class="p-1">${tarjetaElegida.name}</h2>
-      <p class="p-1">Description: ${tarjetaElegida.description}</p>
-      <p class="p-1">Date: ${tarjetaElegida.date}</p>
-      <p class="p-1">Place: ${tarjetaElegida.place}</p>
-      <p class="p-1"> Assistance: ${tarjetaElegida.assistance || tarjetaElegida.estimate}</p>
-      <p class="p-1">Price $${tarjetaElegida.price}</p>
+// // Funciones
+// function tarjetaSeleccionada(){
+//   let tarjetaElegida = events.find(info => info._id == idParams);
+//   let tarjeta = "";
+//   tarjeta += `<img src="${tarjetaElegida.image}" class="card-img-top d-flex" alt="..." height="280" >
+//   </div>
+//   <div class="p-3"></div>
+//   <div class="card justify-content-center border-dark p-2  d-flex">
+//       <h2 class="p-1">${tarjetaElegida.name}</h2>
+//       <p class="p-1">Description: ${tarjetaElegida.description}</p>
+//       <p class="p-1">Date: ${tarjetaElegida.date}</p>
+//       <p class="p-1">Place: ${tarjetaElegida.place}</p>
+//       <p class="p-1"> Assistance: ${tarjetaElegida.assistance || tarjetaElegida.estimate}</p>
+//       <p class="p-1">Price $${tarjetaElegida.price}</p>
       
-`
+// `
 
-contenedorTarjeta.innerHTML = tarjeta;
+// contenedorTarjeta.innerHTML = tarjeta;
 
-}
+// }
 
-async function getEvents () {
-  try {
-      let response = await fetch(urlAPI);
-      let dataAPI = await response.json();
-      events = dataAPI.events;
-      currentDate = dataAPI.currentDate;
-      tarjetaSeleccionada();
-  } catch (error){
-      console.log(error.message);
-  } 
-  };
+// async function getEvents () {
+//   try {
+//       let response = await fetch(urlAPI);
+//       let dataAPI = await response.json();
+//       events = dataAPI.events;
+//       currentDate = dataAPI.currentDate;
+//       tarjetaSeleccionada();
+//   } catch (error){
+//       console.log(error.message);
+//   } 
+//   };
+const { createApp } = Vue
+
+const app = createApp({
+  data(){
+    return {
+      urlAPI:'https://mindhub-xj03.onrender.com/api/amazing',
+      eventos:[],
+      currentDate:'',
+      backupEventos:[],
+      texto:'',
+      categorias:[],
+      categoriasSeleccionadas:[],
+    }
+
+  },
+  created(){
+    this.pedirDatos()
+  },
+  mounted(){
+
+  },
+  methods:{
+    pedirDatos(){
+      fetch(this.urlAPI)
+      .then(response =>response.json())
+      .then(datosAPI =>{
+        this.eventos = datosAPI.events
+        this.backupEventos = this.eventos
+        this.currentDate = datosAPI.currentDate
+        this.extraerCategorias(datosAPI.events)
+      })
+    },
+  },
+  computed:{
+  }
+}).mount('#app')
--- scripts/main-upcomingEvents.js
+++ scripts/main-upcomingEvents.js
@@ -1,126 +1,57 @@
-let urlAPI = 'https://mindhub-xj03.onrender.com/api/amazing';
-let events = [];
-let inputBusqueda = document.querySelector("input[name='textInput']");
-let checkboxes;
-let currentDate = '';
+const { createApp } = Vue
 
-//Constantes Capturadas y variables
-
-const contenedor = document.getElementById('cardsContainer')
-const contenedorChecks = document.getElementById('checkbox-container')
-const input = document.querySelector("input[name='textInput']");
-
-//Eventos 
-
-input.addEventListener('input', superFiltro)
-contenedorChecks.addEventListener('change', superFiltro)
-
-//Llamada de funciones
-
-getEvents();
-
-//Funciones
-async function getEvents () {
-  try {
-      let response = await fetch(urlAPI);
-      let dataAPI = await response.json();
-      events = dataAPI.events;
-      currentDate = dataAPI.currentDate;
-      pintarTarjetas(events)
-      crearCheckboxes(events)
-  } catch (error){
-      console.log(error.message);
-  } 
-  };
-
-function superFiltro(){
-  let arrayFiltrado1 = filtrarPorTexto(events,input.value)
-  let arrayFiltrado2 = filtrarPorCategoria(arrayFiltrado1)
-  pintarTarjetas(arrayFiltrado2)
-}
-
-function pintarTarjetas(arrayDatos){
-  if(arrayDatos.length == 0){
-    contenedor.innerHTML = "<h3 class='display-1-fw-bolder'>No hay coincidencias!</h3>";
-  } else { 
-  let tarjetas = ''
-  let arrayFiltrado = arrayDatos.filter(arrayDatos => arrayDatos.date>currentDate)
-  arrayFiltrado.forEach(elemento =>{
-
-tarjetas +=    `
-<div class="individual-card-container d-flex">
-<div class="card">
-<div class="card-img">
-
-<img
-src="${elemento.image}"
-class="card-img-top col-2"
-alt="..."
-height="250"
-/>
-</div>
-
-<div class="card-body">
-<h5 class="card-title">${elemento.name}</h5>
-<p class="card-text d-flex">${elemento.description}</p>
-<div class="card-price justify-content-between">
-  <p>Price $${elemento.price}</p>
-  <a href="./details.html?id=${elemento._id}" class="btn btn-primary">Ver mas...</a>
-</div>
-</div>
-</div>
-</div>`
-  })
-  contenedor.innerHTML = tarjetas
-}}
-
-function filtrarPorTexto(arrayDatos, texto){
-  let arrayFiltrado = arrayDatos.filter(elemento => elemento.name.toLowerCase().includes(texto.toLowerCase()))
-  return arrayFiltrado
-}
-function crearCheckboxes(arrayInfo){
-  let checks = ''
-  let categoryRepeated = arrayInfo.map(elemento =>elemento.category)
-  let category = new Set(categoryRepeated.sort((a,b) =>{
-    if(a>b){
-      return 1
+const app = createApp({
+  data(){
+    return {
+      urlAPI:'https://mindhub-xj03.onrender.com/api/amazing',
+      eventos:[],
+      currentDate:'',
+      backupEventos:[],
+      texto:'',
+      categorias:[],
+      categoriasSeleccionadas:[],
+      eventosFiltrados:[],
     }
-    if(a<b){
-      return -1
-    }return 0
-    
-  }))
-  category.forEach(elemento => {
-    checks += `<li class="list-group-item">
-    <input
-      class="form-check-input me-1"
-      type="checkbox"
-      value="${elemento}"
-      id="${elemento}"
-    />
-    <label class="form-check-label" for="${elemento}">${elemento}</label>
-  </li>`
-  })
-  contenedorChecks.innerHTML=checks
-}
 
-function filtrarPorCategoria(arrayInfo){
-  let checkboxes = document.querySelectorAll("input[type='checkbox']")
-  let arrayChecks = Array.from(checkboxes)
-  let checksChecked = arrayChecks.filter(check => check.checked)
-  if (checksChecked.length == 0 ){
-    return arrayInfo
+  },
+  created(){
+    this.pedirDatos()
+  },
+  mounted(){
+//   let arrayFiltrado = arrayDatos.filter(arrayDatos => arrayDatos.date>currentDate)
+
+  },
+  methods:{
+    pedirDatos(){
+      fetch(this.urlAPI)
+      .then(response =>response.json())
+      .then(datosAPI =>{
+        this.eventos = datosAPI.events 
+        this.currentDate = datosAPI.currentDate
+        this.eventos = this.eventos.filter(events=>events.date>this.currentDate)
+        this.backupEventos = this.eventos
+        this.extraerCategorias(datosAPI.events)
+      })
+    },
+    extraerCategorias(array){
+      array.forEach(elemento=>{
+        if(!this.categorias.includes(elemento.category)){
+          this.categorias.push(elemento.category)
+        }
+      })
+
+    }
+
+  },
+  computed:{
+    filtroDoble(){
+      let primerFiltro = this.backupEventos.filter(
+        evento => evento.name.toLowerCase().includes(this.texto.toLowerCase()))
+        if(!this.categoriasSeleccionadas.length){
+          this.eventos = primerFiltro
+        }else{
+          this.eventos = primerFiltro.filter(evento=> this.categoriasSeleccionadas.includes(evento.category))
+        }
+    }
   }
-  let checkValues = checksChecked.map(check => check.value)
-console.log(checkValues)
-  let arrayFiltrado = arrayInfo.filter(elemento => checkValues.includes(elemento.category))
-  console.log(arrayFiltrado)
-  return arrayFiltrado
-}
-
-// const date = new Date(); // Getting automatically the current date and formatting it to yyyy-mm-dd
-// let day = date.getDate();
-// let month = date.getMonth() + 1;
-// let year = date.getFullYear();
-// let currentDate = `${year}-${month}-${day}`;
-// console.log(currentDate);
+}).mount('#app')
